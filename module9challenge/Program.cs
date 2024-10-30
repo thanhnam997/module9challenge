5c6fb27726175904e82fe7651316fd8c08c8dca4
Desiree Wright <xv7406ml@go.minneapolis.edu>
2024-10-30 09:21:55 +0000

    initialcommit

---
+++ module9challenge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace VideoGames
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Creating a list of Video Games
+            List<VideoGame> games = new List<VideoGame>();
+            games.Add(new VideoGame { Name = "Resident Evil 5", Genre = "Survival Horror", Rating = 4});
+            games.Add(new VideoGame { Name = "Stardew Valley", Genre = "Cozy Game", Rating = 3.2});
+            games.Add(new VideoGame { Name = "My Time at Portia", Genre = "Adventure", Rating = 3.5});
+            games.Add(new VideoGame { Name = "Cult of the Lamb", Genre = "Dungeon Crawl", Rating = 4});
+            games.Add(new VideoGame { Name = "Baldur's Gate 3", Genre = "Role Playing Game", Rating = 5}); 
+        
+
+            //Use a LINQ query to create a new list that only contains games with a rating of 4 or higher
+            var topRatedGames = (from game in games
+                              where game.Rating >= 4
+                              select game).ToList();
+        
+            //Use orderby LINQ query to sort by game name
+            var sortedGames = (from game in topRatedGames
+                            orderby game.Name
+                            select game).ToList();
+            foreach (var game in sortedGames)
+            {
+                Console.WriteLine(game.Name);
+            }
+        }
+    }
+}
---
+++ module9challenge/VideoGame.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace VideoGames
+{
+    public class VideoGame
+    {
+        //Attributes (properties)
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public double Rating { get; set; }
+
+    
+    }
+}
